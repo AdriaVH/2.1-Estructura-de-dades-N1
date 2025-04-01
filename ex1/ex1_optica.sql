a389203af5c64f3cf51dc430c0ef798ea97df188
Adrià <166712306+AdriaVH@users.noreply.github.com>
2025-04-01 12:05:01 +0200

    update not null properties

--- ex1/ex1_optica.sql
+++ ex1/ex1_optica.sql
@@ -1,163 +1,176 @@
--- MySQL Workbench Forward Engineering
+CREATE DATABASE  IF NOT EXISTS `cul_d_ampolla` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cul_d_ampolla`;
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: cul_d_ampolla
+-- ------------------------------------------------------
+-- Server version	8.4.1
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema cul_d_ampolla
--- -----------------------------------------------------
+--
+-- Table structure for table `clients`
+--
 
--- -----------------------------------------------------
--- Schema cul_d_ampolla
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `cul_d_ampolla` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `cul_d_ampolla` ;
-
--- -----------------------------------------------------
--- Table `cul_d_ampolla`.`employees`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cul_d_ampolla`.`employees` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
+DROP TABLE IF EXISTS `clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clients` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fullName` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  `phoneNumber` int NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `registrationDate` date NOT NULL,
+  `recomendedBy` int DEFAULT NULL,
+  `employeeId` int NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `phoneNumber_UNIQUE` (`phoneNumber`),
+  KEY `id_idx` (`employeeId`),
+  KEY `id_idx1` (`recomendedBy`),
+  CONSTRAINT `fk_employee` FOREIGN KEY (`employeeId`) REFERENCES `employees` (`id`),
+  CONSTRAINT `fk_recomended` FOREIGN KEY (`recomendedBy`) REFERENCES `clients` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `employees`
+--
 
--- -----------------------------------------------------
--- Table `cul_d_ampolla`.`clients`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cul_d_ampolla`.`clients` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `fullName` VARCHAR(45) NOT NULL,
-  `address` VARCHAR(45) NOT NULL,
-  `phoneNumber` INT NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `registrationDate` DATE NOT NULL,
-  `recomendedBy` INT NULL DEFAULT NULL,
-  `employeeId` INT NOT NULL,
+DROP TABLE IF EXISTS `employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employees` (
+  `id` int NOT NULL,
+  `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  UNIQUE INDEX `phoneNumber_UNIQUE` (`phoneNumber` ASC) VISIBLE,
-  INDEX `id_idx` (`employeeId` ASC) VISIBLE,
-  INDEX `id_idx1` (`recomendedBy` ASC) VISIBLE,
-  CONSTRAINT `fk_employee`
-    FOREIGN KEY (`employeeId`)
-    REFERENCES `cul_d_ampolla`.`employees` (`id`),
-  CONSTRAINT `fk_recomended`
-    FOREIGN KEY (`recomendedBy`)
-    REFERENCES `cul_d_ampolla`.`clients` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `glasses`
+--
 
--- -----------------------------------------------------
--- Table `cul_d_ampolla`.`supplier`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cul_d_ampolla`.`supplier` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `address` VARCHAR(45) NOT NULL,
-  `phoneNumber` INT NOT NULL,
-  `faxNumber` INT NOT NULL,
-  `NIF` VARCHAR(45) NOT NULL,
+DROP TABLE IF EXISTS `glasses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `glasses` (
+  `id` int NOT NULL,
+  `brand` varchar(20) NOT NULL,
+  `prescriptionLeft` float NOT NULL,
+  `prescriptionRight` float NOT NULL,
+  `frameType` enum('flotant','pasta','metalica') NOT NULL,
+  `frameColor` varchar(15) NOT NULL,
+  `glassColor` varchar(15) NOT NULL,
+  `price` float NOT NULL,
+  `clientId` int DEFAULT NULL,
+  UNIQUE KEY `ideyeGlasses_UNIQUE` (`id`),
+  UNIQUE KEY `idBrand_UNIQUE` (`brand`),
+  KEY `id_idx` (`clientId`),
+  CONSTRAINT `fk_brand_glasses` FOREIGN KEY (`brand`) REFERENCES `glassesbrand` (`name`),
+  CONSTRAINT `id` FOREIGN KEY (`clientId`) REFERENCES `clients` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `glassesbrand`
+--
+
+DROP TABLE IF EXISTS `glassesbrand`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `glassesbrand` (
+  `brandId` int NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `supplierId` int NOT NULL,
+  PRIMARY KEY (`brandId`),
+  UNIQUE KEY `idglassesBrand_UNIQUE` (`brandId`),
+  UNIQUE KEY `supplierId_UNIQUE` (`supplierId`),
+  UNIQUE KEY `name_UNIQUE` (`name`),
+  CONSTRAINT `fk_brand_supplier` FOREIGN KEY (`supplierId`) REFERENCES `supplier` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `sale`
+--
+
+DROP TABLE IF EXISTS `sale`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sale` (
+  `id` int NOT NULL,
+  `idEmployee` int NOT NULL,
+  `idClient` int NOT NULL,
+  `date` date NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `idsupplier_UNIQUE` (`id` ASC) VISIBLE,
-  UNIQUE INDEX `NIF_UNIQUE` (`NIF` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  UNIQUE KEY `idSale_UNIQUE` (`id`),
+  KEY `id_idx` (`idEmployee`),
+  KEY `id_idx1` (`idClient`),
+  CONSTRAINT `id_client` FOREIGN KEY (`idClient`) REFERENCES `clients` (`id`),
+  CONSTRAINT `id_employee` FOREIGN KEY (`idEmployee`) REFERENCES `employees` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `saledetails`
+--
 
--- -----------------------------------------------------
--- Table `cul_d_ampolla`.`glasses`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cul_d_ampolla`.`glasses` (
-  `id` INT NOT NULL,
-  `idSupplier` INT NOT NULL,
-  `brand` VARCHAR(45) NOT NULL,
-  `prescriptionLeft` FLOAT NOT NULL,
-  `prescriptionRight` FLOAT NOT NULL,
-  `frameType` ENUM('flotant', 'pasta', 'metalica') NOT NULL,
-  `frameColor` VARCHAR(15) NOT NULL,
-  `glassColor` VARCHAR(15) NOT NULL,
-  `price` FLOAT NOT NULL,
-  `clientId` INT NOT NULL,
-  UNIQUE INDEX `ideyeGlasses_UNIQUE` (`id` ASC) VISIBLE,
-  UNIQUE INDEX `supplier_UNIQUE` (`idSupplier` ASC) VISIBLE,
-  INDEX `id_idx` (`clientId` ASC) VISIBLE,
-  CONSTRAINT `idSupplier`
-    FOREIGN KEY (`idSupplier`)
-    REFERENCES `cul_d_ampolla`.`supplier` (`id`),
-  CONSTRAINT `id`
-    FOREIGN KEY (`clientId`)
-    REFERENCES `cul_d_ampolla`.`clients` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `cul_d_ampolla`.`sale`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cul_d_ampolla`.`sale` (
-  `id` INT NOT NULL,
-  `idEmployee` INT NOT NULL,
-  `idClient` INT NOT NULL,
-  `date` DATE NOT NULL,
+DROP TABLE IF EXISTS `saledetails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `saledetails` (
+  `id` int NOT NULL,
+  `saleId` int NOT NULL,
+  `glassesId` int NOT NULL,
+  `priceGlasses` double NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `idSale_UNIQUE` (`id` ASC) VISIBLE,
-  INDEX `id_idx` (`idEmployee` ASC) VISIBLE,
-  INDEX `id_idx1` (`idClient` ASC) VISIBLE,
-  CONSTRAINT `id_employee`
-    FOREIGN KEY (`idEmployee`)
-    REFERENCES `cul_d_ampolla`.`employees` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `id_client`
-    FOREIGN KEY (`idClient`)
-    REFERENCES `cul_d_ampolla`.`clients` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `id_idx` (`saleId`),
+  KEY `id_idx1` (`glassesId`),
+  CONSTRAINT `id_glasses` FOREIGN KEY (`glassesId`) REFERENCES `glasses` (`id`),
+  CONSTRAINT `id_sale` FOREIGN KEY (`saleId`) REFERENCES `sale` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `supplier`
+--
 
--- -----------------------------------------------------
--- Table `cul_d_ampolla`.`saleDetails`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cul_d_ampolla`.`saleDetails` (
-  `id` INT NOT NULL,
-  `saleId` INT NOT NULL,
-  `glassesId` INT NOT NULL,
-  `priceGlasses` DOUBLE NOT NULL,
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+DROP TABLE IF EXISTS `supplier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `supplier` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  `phoneNumber` int NOT NULL,
+  `faxNumber` int NOT NULL,
+  `NIF` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `id_idx` (`saleId` ASC) VISIBLE,
-  INDEX `id_idx1` (`glassesId` ASC) VISIBLE,
-  CONSTRAINT `id_sale`
-    FOREIGN KEY (`saleId`)
-    REFERENCES `cul_d_ampolla`.`sale` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `id_glasses`
-    FOREIGN KEY (`glassesId`)
-    REFERENCES `cul_d_ampolla`.`glasses` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  UNIQUE KEY `idsupplier_UNIQUE` (`id`),
+  UNIQUE KEY `NIF_UNIQUE` (`NIF`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- Dump completed on 2025-04-01 11:54:05
